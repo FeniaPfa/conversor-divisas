0572c5cf8d9ce5abfc82d7b8cfe5771f4378d774
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2022-11-15 18:21:30 -0300

    initial commit : base code

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const clpInput = document.getElementById("clpInput");
+const selectMonedas = document.getElementById("selectMonedas");
+const btnSubmit = document.getElementById("btnSubmit");
+const URL = "https://mindicador.cl/api/"
